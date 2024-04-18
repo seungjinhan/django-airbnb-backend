88eff1643d8d745c6adb5fcc559a877e8efd7307
seungjinhan <hanblues@gmail.com>
2024-04-18 22:28:12 +0800

    url

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
 
-# Create your views here.
+
+def see_all_rooms(req):
+    rooms = Room.objects.all()
+    return render(
+        req,
+        "all_rooms.html",
+        {"rooms": rooms, "title": "Hello This is django"},
+    )
+
+
+def see_one_rooms(req, room_pk):
+    try:
+        room = Room.objects.get(pk=room_pk)
+        return render(
+            req,
+            "room_detail.html",
+            {"room": room},
+        )
+    except Room.DoesNotExist:
+        return render(req, "room_detail.html", {"not_found": True})
