8af13944a3aa3426bbc264b79c8067b93adf00c8
seungjinhan <hanblues@gmail.com>
2024-04-18 19:33:59 +0800

    admin filter custom

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,8 +1,37 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from .models import Review
+
+
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words"
+    parameter_name = "word"
+
+    def lookups(self, request: Any, model_admin: Any) -> list[tuple[Any, str]]:
+        return [
+            ("ddd", "Ddd"),
+            ("great", "Great"),
+        ]
+
+    def queryset(self, request: Any, queryset: QuerySet[Any]) -> QuerySet[Any] | None:
+        word = self.value()
+        if word == None:
+            return queryset
+
+        return queryset.filter(payload__contains=word)
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'payload')
-    list_filter = ('rating',)
+    list_display = (
+        "__str__",
+        "payload",
+    )
+    list_filter = (
+        WordFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+    )
