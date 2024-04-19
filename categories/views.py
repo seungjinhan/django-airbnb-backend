58dba44d4711bc9745dea1ae21318fee86b785c4
seungjinhan <hanblues@gmail.com>
2024-04-19 14:55:47 +0800

    django rest framework basic

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+@api_view(["GET", "POST"])
+def categories(req):
+    if req.method == "GET":
+        all = Category.objects.all()
+        srz = CategorySerializer(all, many=True)
+        return Response(srz.data)
+    elif req.method == "POST":
+        srz = CategorySerializer(data=req.data)
+        if srz.is_valid():
+            new_data = srz.save()
+            return Response(CategorySerializer(new_data).data)
+        else:
+            return Response(srz.error)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def category(req, pk):
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if req.method == "GET":
+        return Response(CategorySerializer(category).data)
+    elif req.method == "PUT":
+        serializer = CategorySerializer(
+            category,
+            data=req.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            update_date = serializer.save()
+            return Response(CategorySerializer(update_date).data)
+        else:
+            return Response(serializer.errors)
+    elif req.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
+
 CUSTOM_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
@@ -50,7 +54,7 @@ SYSTEM_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
-INSTALLED_APPS = CUSTOM_APPS + SYSTEM_APPS
+INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
