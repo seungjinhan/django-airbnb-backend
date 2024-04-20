7f0cde566c51d067a925849fe4ee756d52f184da
seungjinhan <hanblues@gmail.com>
2024-04-20 22:46:30 +0800

    Room Serializer

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,8 +1,34 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Amenity
+from .models import Amenity, Room
+from users.serializer import TinyUserSerializer
 
 
 class AmenitySerializer(ModelSerializer):
     class Meta:
         model = Amenity
+        fields = (
+            "name",
+            "description",
+        )
+
+
+class RoomDetailSerializer(ModelSerializer):
+    owner = TinyUserSerializer()
+    amenities = AmenitySerializer(many=True)
+
+    class Meta:
+        model = Room
         fields = "__all__"
+        # depth = 1
+
+
+class RoomListSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = (
+            "pk",
+            "name",
+            "country",
+            "city",
+            "price",
+        )
--- rooms/views.py
+++ rooms/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
-from .models import Amenity
-from .serializers import AmenitySerializer
+from .models import Amenity, Room
+from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
 
 
 class Amenities(APIView):
@@ -47,6 +47,26 @@ class AmenityDetail(APIView):
         amenity = self.get_object(pk)
         amenity.delete()
         return Response(status=HTTP_204_NO_CONTENT)
+
+
+class Rooms(APIView):
+    def get(self, req):
+        all_data = Room.objects.all()
+        serializer = RoomListSerializer(all_data, many=True)
+        return Response(serializer.data)
+
+
+class RoomDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def get(self, req, pk):
+        data = self.get_object(pk)
+        serializer = RoomDetailSerializer(data)
+        return Response(serializer.data)
 
 
 # from django.shortcuts import render
