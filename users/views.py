d4eaf1ec218b9a8c90b378120d4c55a110c58788
seungjinhan <hanblues@gmail.com>
2024-04-22 19:17:00 +0800

    chagne password

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from rest_framework import status, exceptions
 from rest_framework.permissions import IsAuthenticated
 
 from .serializer import PrivateUserSerializer
+from .models import User
 
 
 class Me(APIView):
@@ -26,6 +27,35 @@ class Me(APIView):
             return Response(srz.data)
         else:
             return Response(srz.errors)
+
+
+class PublicUser(APIView):
+    def get(self, req, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise exceptions.NotFound
+
+        srz = PrivateUserSerializer(user)
+        return Response(srz.data)
+
+
+class ChangePassword(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, req):
+        user = req.user
+        old_pw = req.data.get("old_password")
+        new_pw = req.data.get("new_password")
+        if not old_pw or not new_pw:
+            raise exceptions.ParseError
+
+        if user.check_password(old_pw):
+            user.set_password(new_pw)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class Users(APIView):
