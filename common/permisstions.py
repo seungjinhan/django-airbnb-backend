df8dba13b5e6a4fd84717ce950eb10abe3c8d0d3
seungjinhan <hanblues@gmail.com>
2024-04-23 13:45:30 +0800

    permission -grapphql

---
+++ common/permisstions.py
@@ -0,0 +1,13 @@
+import typing
+
+from strawberry.types import Info
+from strawberry.permission import BasePermission
+
+
+class OnlyLoggedIn(BasePermission):
+    message = "You need to be logged in for this!"
+
+    def has_permission(
+        self, source: typing.Any, info: Info
+    ) -> bool | typing.Awaitable[bool]:
+        return info.context.request.user.is_authenticated
--- rooms/schema.py
+++ rooms/schema.py
@@ -1,11 +1,17 @@
-import strawberry
 import typing
+
+import strawberry
+
 from . import types, queries
+from common.permisstions import OnlyLoggedIn
 
 
 @strawberry.type
 class Query:
     all_rooms: typing.List[types.RoomType] = strawberry.field(
         resolver=queries.get_all_rooms,
+        permission_classes=[OnlyLoggedIn],
     )
-    room: typing.Optional[types.RoomType] = strawberry.field(resolver=queries.get_room)
+    room: typing.Optional[types.RoomType] = strawberry.field(
+        resolver=queries.get_room,
+    )
