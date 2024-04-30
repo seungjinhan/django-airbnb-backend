84981bf0b9b15511e3aa9cddacce82ce3ce4050d
seungjinhan <hanblues@gmail.com>
2024-04-30 14:10:46 +0800

    amenity category add pk

--- categories/views.py
+++ categories/views.py
@@ -11,7 +11,9 @@ from .serializers import CategorySerializer
 
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
-    queryset = Category.objects.all()
+    queryset = Category.objects.filter(
+        kind=Category.CategoryKindChoices.ROOMS,
+    )
 
 
 # class Categories(APIView):
